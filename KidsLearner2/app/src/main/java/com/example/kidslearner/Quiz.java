ad69457661a714793571b729814c0fe8f5f2a73e
Muhammad Saad <muhammadsaad6345@gmail.com>
2022-06-17 21:44:57 +0500

    Quiz Layout Added

--- KidsLearner2/app/src/main/java/com/example/kidslearner/Quiz.java
+++ KidsLearner2/app/src/main/java/com/example/kidslearner/Quiz.java
@@ -3,12 +3,25 @@ package com.example.kidslearner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 public class Quiz extends AppCompatActivity {
 
+    Switch btnCat, btnJeep, btnHen;
+    Button submit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        btnCat = findViewById(R.id.btnCat);
+        btnHen = findViewById(R.id.btnHen);
+        btnJeep = findViewById(R.id.btnJeep);
+        submit = findViewById(R.id.btnSubmit);
+
+
     }
 }
