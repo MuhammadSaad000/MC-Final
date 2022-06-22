8fe50fb2b151c4f95c9663c5424d7c1e1ea51f17
Muhammad Saad <muhammadsaad6345@gmail.com>
2022-06-22 17:50:06 +0500

    Custom list row added

--- ListViews/app/src/main/java/com/example/listviews/Task.java
+++ ListViews/app/src/main/java/com/example/listviews/Task.java
@@ -1,2 +1,29 @@
-package com.example.listviews;public class Task {
+package com.example.listviews;
+
+public class Task {
+
+    String TaskHeading;
+    String TaskDetail;
+
+    public Task(String taskHeading, String taskDetail) {
+
+        TaskHeading = taskHeading;
+        TaskDetail = taskDetail;
+    }
+
+    public void setTaskHeading(String taskHeading) {
+        TaskHeading = taskHeading;
+    }
+
+    public void setTaskDetail(String taskDetail) {
+        TaskDetail = taskDetail;
+    }
+
+    public String getTaskHeading() {
+        return TaskHeading;
+    }
+
+    public String getTaskDetail() {
+        return TaskDetail;
+    }
 }
