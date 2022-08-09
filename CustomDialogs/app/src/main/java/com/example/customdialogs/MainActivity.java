970edb9bec980b17b27a2e66982569dfc5b1f9df
Muhammad Saad <muhammadsaad6345@gmail.com>
2022-08-09 16:41:05 +0500

    OnClickListener Added

--- CustomDialogs/app/src/main/java/com/example/customdialogs/MainActivity.java
+++ CustomDialogs/app/src/main/java/com/example/customdialogs/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.customdialogs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnOne;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnOne = findViewById(R.id.btnOne);
+        btnOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
+
     }
 }
