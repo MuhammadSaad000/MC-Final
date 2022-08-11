77ac147b6c01573c681529ace34a999878fd5a8d
Muhammad Saad <muhammadsaad6345@gmail.com>
2022-08-11 18:01:28 +0500

    Buttons and Array Added

--- TodoApp/app/src/main/java/com/example/todoapp/MainActivity.java
+++ TodoApp/app/src/main/java/com/example/todoapp/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.todoapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText task;
+    Button btnAddTask;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ArrayList<String> tasks = new ArrayList<>();
+
+        btnAddTask = findViewById(R.id.btnAddTask);
+        task = findViewById(R.id.taskDetail);
+
+        
     }
 }
