bff69780b5b75d3b13d588053a2d9f2d1e192168
Muhammad Saad <muhammadsaad6345@gmail.com>
2022-09-12 22:43:04 +0500

    Helper added

---
+++ CRUDApp/app/src/main/java/com/example/crud_app/MyDbHelper.java
@@ -0,0 +1,26 @@
+package com.example.crud_app;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class MyDbHelper extends SQLiteOpenHelper {
+
+
+    public MyDbHelper(@Nullable Context context) {
+        super(context, "students.db", null , 1);
+        
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
