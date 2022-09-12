92b799c5ed0842f14705b4e283701760f3cec9ab
Muhammad Saad <muhammadsaad6345@gmail.com>
2022-09-12 17:42:27 +0500

    Backend completed

--- TemperatureConverter/app/src/main/java/com/example/temperatureconverter/MainActivity.java
+++ TemperatureConverter/app/src/main/java/com/example/temperatureconverter/MainActivity.java
@@ -14,11 +14,49 @@ import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button submit;
+    EditText inTemp;
+    ToggleButton toggle;
+    Double a;
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inTemp=(EditText) findViewById(R.id.editText);
+        submit=(Button) findViewById(R.id.button);
+        toggle=(ToggleButton) findViewById(R.id.toggleButton);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(inTemp.getText().toString().isEmpty())
+                {
+                    Toast.makeText(MainActivity.this,"Please enter the temperature",Toast.LENGTH_SHORT).show();
+                }
+
+                else if(toggle.isChecked())
+                {
+                    a=Double.parseDouble(String.valueOf(inTemp.getText()));
+                    Double b=a*9/5+32;
+                    String r=String.valueOf(b);
+                    Toast.makeText(MainActivity.this,r+"°F",Toast.LENGTH_SHORT).show();
+                }
+
+                else
+                {
+                    a=Double.parseDouble(String.valueOf(inTemp.getText()));
+                    Double b=a-32;
+                    Double c=b*5/9;
+                    String r=String.valueOf(c);
+                    Toast.makeText(MainActivity.this,r+"°C",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
 
     }
 
