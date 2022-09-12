915641152844e100880e48081b4feca4ea7cb36c
Muhammad Saad <muhammadsaad6345@gmail.com>
2022-09-12 22:08:26 +0500

    Layout completed

--- CRUDApp/app/src/main/java/com/example/crud_app/MainActivity.java
+++ CRUDApp/app/src/main/java/com/example/crud_app/MainActivity.java
@@ -4,18 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.example.crud_app.data.MyDbHandler;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-        MyDbHandler db = new MyDbHandler(MainActivity.this);
-
-
     }
 }
